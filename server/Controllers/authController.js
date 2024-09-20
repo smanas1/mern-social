9dd053936046b720e37d35905073aca070da21f9
Anas <anasolin01777@gmail.com>
2024-09-20 20:38:20 +0600

    existing user error fix

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -1,16 +1,27 @@
 import { User } from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 export const registerController = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const userinfo = new User({ username, email, password });
 
-    await userinfo.save();
+    const existingUser = await User.findOne({ email: email });
 
-    res.send(userinfo);
+    if (existingUser) return res.status(400).send("Email already exists");
+
+    bcrypt.hash(password, 8, async function (err, hash) {
+      const userinfo = new User({ username, email, password: hash });
+
+      await userinfo.save();
+
+      res.send(userinfo);
+      if (err) {
+        return res.status(500).send("Encryption failed");
+      }
+    });
   } catch (error) {
-    console.log(error);
     if (error.code === 11000) {
+      console.log(error.message);
       return res.status(400).send("Email already exists");
     }
   }
